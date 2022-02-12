7f508a7752ed7f5aa312a4f787b88e1805d86295
Dinashrr <71688294+Dinashrr@users.noreply.github.com>
2022-02-12 21:14:41 +0530

    exp_informer.py

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -61,7 +61,7 @@ class Exp_Informer(Exp_Basic):
         args = self.args
 
         data_dict = {
-            'wind':Dataset_ETT_hour,
+            'wind':Dataset_Custom,
             'ETTh1':Dataset_ETT_hour,
             'ETTh2':Dataset_ETT_hour,
             'ETTm1':Dataset_ETT_minute,
